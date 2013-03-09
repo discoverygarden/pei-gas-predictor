50643d3aba6a595c9f6dfac694489cbfa161a869
Jordan Dukart <jordan@discoverygarden.ca>
2013-03-09 18:02:44 -0400

    Change test route to IRAC and modify IRAC query.

--- routes/irac.js
+++ routes/irac.js
@@ -1,5 +1,5 @@
 var query = require('../mongify/query_gas.js');
 
 exports.list = function(req, res) {
-    query.query_mongo('reality', '127.3', res);
+    query.query_mongo('reality', 'IRAC', res);
 };
